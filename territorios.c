17ee5cd1db6f5bfcb041298513e0c7bee3f3be9c
Evandro Barros <evandrobarrosb98@gmail.com>
2025-10-06 14:32:04 -0300

    nova-funcionalidades

--- territorios.c
+++ territorios.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct Territorio {
     char nome[30];
@@ -7,39 +9,96 @@ struct Territorio {
     int tropas;
 };
 
+void cadastrarTerritorios(struct Territorio* mapa, int qtd) {
+    for (int i = 0; i < qtd; i++) {
+        printf("\nCadastro do território %d:\n", i + 1);
+        printf("Nome: ");
+        scanf("%s", mapa[i].nome);
+        printf("Cor do exército: ");
+        scanf("%s", mapa[i].cor);
+        printf("Quantidade de tropas: ");
+        scanf("%d", &mapa[i].tropas);
+    }
+}
+
+void exibirTerritorios(struct Territorio* mapa, int qtd) {
+    printf("\n=== LISTA DE TERRITÓRIOS ===\n");
+    for (int i = 0; i < qtd; i++) {
+        printf("%d. Nome: %s | Cor: %s | Tropas: %d\n", i + 1, mapa[i].nome, mapa[i].cor, mapa[i].tropas);
+    }
+}
+
+void atacar(struct Territorio* atacante, struct Territorio* defensor) {
+    if (strcmp(atacante->cor, defensor->cor) == 0) {
+        printf("\nVocê não pode atacar um território da mesma cor!\n");
+        return;
+    }
+
+    if (atacante->tropas <= 1) {
+        printf("\nO território atacante não tem tropas suficientes para atacar!\n");
+        return;
+    }
+
+    int dadoAtacante = (rand() % 6) + 1;
+    int dadoDefensor = (rand() % 6) + 1;
+
+    printf("\nAtaque: %s (dado %d) x %s (dado %d)\n", atacante->nome, dadoAtacante, defensor->nome, dadoDefensor);
+
+    if (dadoAtacante > dadoDefensor) {
+        printf("O atacante venceu!\n");
+        strcpy(defensor->cor, atacante->cor);
+        defensor->tropas = atacante->tropas / 2;
+        atacante->tropas = atacante->tropas / 2;
+    } else {
+        printf("O defensor resistiu ao ataque!\n");
+        atacante->tropas -= 1;
+    }
+}
+
+void liberarMemoria(struct Territorio* mapa) {
+    free(mapa);
+}
+
 int main() {
-    struct Territorio territorios[5];
-    int i;
+    srand(time(NULL));
 
-    printf("=== CADASTRO DE TERRITÓRIOS ===\n\n");
+    int qtd;
+    printf("Quantos territórios deseja cadastrar? ");
+    scanf("%d", &qtd);
 
-    for (i = 0; i < 5; i++) {
-        printf("Cadastro do território %d:\n", i + 1);
-        fflush(stdin);
-
-        printf("Digite o nome do território: ");
-        fgets(territorios[i].nome, sizeof(territorios[i].nome), stdin);
-        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = '\0';
-
-        printf("Digite a cor do exército: ");
-        fgets(territorios[i].cor, sizeof(territorios[i].cor), stdin);
-        territorios[i].cor[strcspn(territorios[i].cor, "\n")] = '\0';
-
-        printf("Digite a quantidade de tropas: ");
-        scanf("%d", &territorios[i].tropas);
-        getchar();
-        printf("\n");
+    struct Territorio* mapa = (struct Territorio*) calloc(qtd, sizeof(struct Territorio));
+    if (mapa == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1;
     }
 
-    printf("=== LISTA DE TERRITÓRIOS CADASTRADOS ===\n\n");
+    cadastrarTerritorios(mapa, qtd);
 
-    for (i = 0; i < 5; i++) {
-        printf("Território %d:\n", i + 1);
-        printf("Nome: %s\n", territorios[i].nome);
-        printf("Cor do Exército: %s\n", territorios[i].cor);
-        printf("Quantidade de Tropas: %d\n", territorios[i].tropas);
-        printf("----------------------------------------\n");
-    }
+    int opcao;
+    do {
+        exibirTerritorios(mapa, qtd);
+        printf("\n1 - Atacar\n0 - Sair\nEscolha: ");
+        scanf("%d", &opcao);
+
+        if (opcao == 1) {
+            int iAtacante, iDefensor;
+            printf("Escolha o número do território atacante: ");
+            scanf("%d", &iAtacante);
+            printf("Escolha o número do território defensor: ");
+            scanf("%d", &iDefensor);
+
+            if (iAtacante < 1 || iAtacante > qtd || iDefensor < 1 || iDefensor > qtd) {
+                printf("Opção inválida!\n");
+            } else if (iAtacante == iDefensor) {
+                printf("Um território não pode atacar a si mesmo!\n");
+            } else {
+                atacar(&mapa[iAtacante - 1], &mapa[iDefensor - 1]);
+            }
+        }
+    } while (opcao != 0);
+
+    liberarMemoria(mapa);
+    printf("\nMemória liberada. Encerrando o programa.\n");
 
     return 0;
 }
